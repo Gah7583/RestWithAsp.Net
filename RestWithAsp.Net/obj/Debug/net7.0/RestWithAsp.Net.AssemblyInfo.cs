c40ffe9108b2c4e512914e67dca608d75744102e
Luis Gabriel Laurindo Mauricio <gah7583@gmail.com>
2024-01-15 00:24:58 -0300

    Docker

--- RestWithAsp.Net/obj/Debug/net7.0/RestWithAsp.Net.AssemblyInfo.cs
+++ RestWithAsp.Net/obj/Debug/net7.0/RestWithAsp.Net.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2081f678-c881-48e7-add8-324ee9351612")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestWithAsp.Net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
